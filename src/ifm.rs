79896e3b1d0808b66146be0de8b39fca307de8cd
Cappy Ishihara <cappy@cappuchino.xyz>
2022-04-28 18:22:22 +0700

    clean up debugging code

--- src/ifm.rs
+++ src/ifm.rs
@@ -3,7 +3,7 @@
 
 use crate::ifm_data::*;
 use gdnative::prelude::*;
-use std::{net::UdpSocket};
+use std::net::UdpSocket;
 
 const RUN_FACE_TRACKER_TEXT: &str = "run_face_tracker";
 const STOP_FACE_TRACKER_TEXT: &str = "stop_face_tracker";
@@ -18,7 +18,7 @@ pub struct Ifacialmocap {
     server: Option<UdpSocket>,
     data_map: Dictionary, // -> IfacialmocapData
 
-    // TODO: Replace this with Godot's UDPServer class, which is another level of hell.
+                          // TODO: Replace this with Godot's UDPServer class, which is another level of hell.
 }
 
 #[methods]
@@ -33,26 +33,21 @@ impl Ifacialmocap {
 
     #[export]
     fn _ready(&mut self, _owner: &Node) {
-        godot_print!("hello, world.");
-        self.start_reciever(_owner);
-        self.stop_reciever(_owner);
+        godot_print!("iFM Tracker ready!");
     }
     fn register_signals(builder: &ClassBuilder<Self>) {
-        builder.signal(&RUN_FACE_TRACKER_TEXT)
-            .done();
-        builder.signal(&STOP_FACE_TRACKER_TEXT)
-            .done();
+        builder.signal(&RUN_FACE_TRACKER_TEXT).done();
+        builder.signal(&STOP_FACE_TRACKER_TEXT).done();
     }
     #[export]
     fn _process(&mut self, _owner: &Node, _delta: f64) {
-        if self.is_listening(_owner){
+        if self.is_listening(_owner) {
             let mut buf = [0; 2048];
             let (size, _origin) = self.server.as_ref().unwrap().recv_from(&mut buf).unwrap();
             let _packet = self.server.as_ref().unwrap().recv_from(&mut buf).unwrap();
             self.data_map =
-                IfacialmocapData::from_str(std::str::from_utf8(&buf[..size]).unwrap())
-                .as_dict();
-            godot_dbg!("{:#?}", &self.data_map);
+                IfacialmocapData::from_str(std::str::from_utf8(&buf[..size]).unwrap()).as_dict();
+            //godot_dbg!("{:#?}", &self.data_map);
         }
     }
     fn _stop_tracker(&mut self) {
